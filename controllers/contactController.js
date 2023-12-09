ea78698b8e991ee76d37caec52e9400684ae2982
ronnie <ronnieineza@gmail.com>
2023-12-09 16:00:18 +0300

    have a functional smpt server running

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -43,31 +43,38 @@ require('dotenv').config();
 // });
 
 async function sendContactFormEmail(req, res) {
-	const { email, phoneNumber, company, name, message } = req.body;
-	console.log('Email sent\n\n', email, '\n\n');
+	const { email, phone, company, name, message } = req.body;
+	// console.log('Email sent\n\n', email, '\n\n');
+	// console.log('Phone sent\n\n', phone, '\n\n');
+	// console.log('Company sent\n\n', company, '\n\n');
+	// console.log('Name sent\n\n', name, '\n\n');
+	// console.log('Message sent\n\n', message, '\n\n');
 	const smtpTransport = nodemailer.createTransport({
-		service: 'Gmail',
+		host: 'smtp-relay.brevo.com',
+		port: 587,
+		secure: false,
 		auth: {
-			user: process.env.EMAIL_USER,
-			pass: process.env.EMAIL_PASSWORD,
+			user: process.env.API_USER,
+			pass: process.env.API_PASSWORD,
 		},
 	});
 
+	const subject = `New Brownies Website message from ${name}`;
 	const mailOptions = {
 		from: email,
 		// to: 'recipient@example.com', // Replace with the recipient's email
 		to: 'peyivix781@lanxi8.com', // Replace with the recipient's email
-		subject: 'New Contact Form Submission',
-		text: `Name: ${name}\nCompany: ${company}\nEmail: ${email}\nPhone Number: ${phoneNumber}\nMessage: ${message}`,
+		subject: subject,
+		text: `Name: ${name}\nCompany: ${company}\nEmail: ${email}\nPhone Number: ${phone}\nMessage: ${message}`,
 	};
 
 	try {
 		await smtpTransport.sendMail(mailOptions);
 		console.log('Email sent successfully');
-		return true;
+		res.status(200).json({ success: true, message: 'Email sent successfully' });
 	} catch (error) {
 		console.error('Error sending email:', error);
-		return false;
+		res.status(500).json({ success: false, message: 'Error sending email' });
 	}
 }
 
